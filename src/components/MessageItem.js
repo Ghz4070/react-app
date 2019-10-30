27b460741cdc5055d7e823fc2241882c3924ce8a
Ghz4070 <ghezal-ilies@outlook.fr>
2019-10-30 16:07:56 +0100

    display username with api

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Emoji from 'react-emoji-render';
 
 const styleLi = { marginBottom: 10, backgroundColor: "lightblue", padding: 10, borderRadius: 10 }
+const styleSpan = { marginBottom: 10, padding: 10 }
 
 const MessageItem = (props) => {
     console.log(props)
     return (
         <li style={styleLi}>
-            {/** <span>@{props.message.user}</span> **/}
+            <span style={styleSpan}>@{props.message.username} :</span>
             <Emoji text={props.message.message} />
         </li>
     );

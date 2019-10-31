9c621269ac22cb049c0fc6974975314d713c3297
Ghz4070 <ghezal-ilies@outlook.fr>
2019-10-31 17:55:06 +0100

    correction

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux'
-import { store } from '../redux';
+import { store } from '../redux/store';
 import Chat from './Chat';
 import SettingsContainer from './../containers/SettingsContainer';
 import {
